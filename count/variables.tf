d959fa44cae047a3d9a6ca4e0ffdd8eba14e4924
Ravikumar-code-maker <ravikumarchinthapally@gmail.com>
2026-04-02 14:12:14 +0530

    Update variables.tf

--- count/variables.tf
+++ count/variables.tf
@@ -1,12 +1,15 @@
 variable "project_id" {
+  type = string
   description = "GCP Project ID"
 }
 
 variable "region" {
+  type = string
   description = "GCP Region"
 }
 
 variable "zone" {
+  type = string
   description = "GCP Zone"
 }
 
