27965916f8ecf29470d4e774f50cca312fe0585f
Ravikumar-code-maker <ravikumarchinthapally@gmail.com>
2026-04-02 14:10:51 +0530

    Update variables.tf

--- count/variables.tf
+++ count/variables.tf
@@ -1,1 +1,18 @@
+variable "project_id" {
+  description = "GCP Project ID"
+}
 
+variable "region" {
+  description = "GCP Region"
+}
+
+variable "zone" {
+  description = "GCP Zone"
+}
+
+# For count method
+variable "instance_count" {
+  description = "Number of instances"
+  type        = number
+  default     = 0
+}
