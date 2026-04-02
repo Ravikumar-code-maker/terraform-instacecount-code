3982525c4b910b4a50aa1ff45278d948ea0eccf5
Ravikumar-code-maker <ravikumarchinthapally@gmail.com>
2026-04-02 14:23:51 +0530

    Update main.tf

--- for_each/main.tf
+++ for_each/main.tf
@@ -1,10 +1,19 @@
-terraform {
-  required_version = ">= 1.3.0"
+resource "google_compute_instance" "vm" {
+  for_each = var.instances
 
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "~> 4.0"
+  name         = each.value.name
+  machine_type = each.value.machine_type
+  zone         = var.zone
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
     }
   }
+
+  network_interface {
+    network = "default"
+    access_config {}
+  }
 }
+
