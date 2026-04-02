07421ea2c4f3432c435f3b40228d72d41f027917
Ravikumar-code-maker <ravikumarchinthapally@gmail.com>
2026-04-02 14:20:27 +0530

    Update variables.tf

--- for_each/variables.tf
+++ for_each/variables.tf
@@ -1,1 +1,14 @@
+project_id     = "my-project-id"
+region         = "us-central1"
+zone           = "us-central1-a"
+
+# For for_each method
+variable "instances" {
+  description = "Map of instances"
+  type = map(object({
+    name         = string
+    machine_type = string
+  }))
+  default = {}
+}
 
