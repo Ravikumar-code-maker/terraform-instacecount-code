1b84fe0159e41d131da71429379a6c8170e1c6e7
Ravikumar-code-maker <ravikumarchinthapally@gmail.com>
2026-04-02 14:15:36 +0530

    Update main.tf

--- count/main.tf
+++ count/main.tf
@@ -1,1 +1,17 @@
+resource "google_compute_instance" "vm" {
+  count        = var.instance_count
+  name         = "vm-${count.index}"
+  machine_type = "e2-medium"
+  zone         = var.zone
 
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {}
+  }
+}
